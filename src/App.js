cecff6d1943e11082c596677d52ec2f2b0ac3b74
김정준 <mabushii68@gmail.com>
2025-09-29 00:58:32 +0900

    2025 09 29 React Commit 001

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import Book from './Book';
-import Datas from './Datas';
-import "./App.css";
+import { useState } from "react";
+import Datas from "./Datas";
 
-function App(){
+function App() {
+  const [current, setCurrent] = useState(0);
+
   return (
-    <section className="booklist">
-      {Datas.map((data) => {
-         return <Book key={data.id} data = {data} /> 
-        }
-      ) }
-    </section>
-  )
+    <div style={{ padding: 20 }}>
+      <div style={{ display: "flex", gap: 8, marginBottom: 16 }}>
+        {Datas.map((d, idx) => (
+          <button
+            key={d.tab}
+            onClick={() => setCurrent(idx)}
+            style={{
+              padding: "6px 12px",
+              borderRadius: 8,
+              border: "1px solid #cbd5e1",
+              background: current === idx ? "#e0ecff" : "#fff",
+              cursor: "pointer"
+            }}
+          >
+            {d.tab} {idx}
+          </button>
+        ))}
+      </div>
+
+      <div style={{ border: "1px solid #e5e7eb", borderRadius: 12, padding: 16 }}>
+        {Datas[current].content}
+      </div>
+    </div>
+  );
 }
+
 export default App;
--- src/Datas.js
+++ src/Datas.js
@@ -1,26 +1,17 @@
-const Datas = [
-    {
-      id: 1,
-      title: 'Clean Code',
-      author: '로버트 C. 마틴',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolor natus sit atque corrupti pariatur quaerat? Laudantium reprehenderit dolor tempore.',
-    },
-    {
-      id: 2,
-      title: '실용주의 프로그래머 20주년 기념판',
-      author: '데이비드 토머스, 앤드류 헌트',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolor natus sit atque corrupti pariatur quaerat? Laudantium reprehenderit dolor tempore.',
-    },
+import Web from "./Web";
+import Html from "./Html";
+import Css from "./Css";
+import JavaScript from "./JavaScript";
+import ReactJs from "./ReactJs";
+import NodeJs from "./NodeJs";
 
-     {
-      id: 3,
-      title: '밑바닥부터 만드는 컴퓨팅 시스템',
-      author: '노암 니산, 시몬 쇼켄',
-      description:
-       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolor natus sit atque corrupti pariatur quaerat? Laudantium reprehenderit dolor tempore.',
-    },
-]
-       
-export default Datas ; 
+const Datas = [
+  { tab: "WEB",        content: (<Web />) },
+  { tab: "HTML",       content: (<Html />) },
+  { tab: "CSS",        content: (<Css />) },
+  { tab: "JavaScript", content: (<JavaScript />) },
+  { tab: "ReactJs",    content: (<ReactJs />) },
+  { tab: "NodeJs",     content: (<NodeJs />) },
+];
+
+export default Datas;
